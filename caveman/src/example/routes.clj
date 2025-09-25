282eca939bb9e3e8e35b0bd5e03a6061a8ed0c75
Mario <developer@theboywhoroared.com>
2025-09-25 14:10:44 -0400

    18 Group route handlers by page
    Refactors the route handlers by page/feature. The routes are moved into
    namespaces based on the page they will serve.
    
    We want as few routes as a possible to be used for serving multiple
    pages. Ideally no route will serve multipe pages. If this step isn't
    taken early, it can become a hairball.
    
    The new route namespaces expose function that returns a collection of
    routes (with handlers), rather than needing to build up a tree of routes
    from accross the different namespaces.
    
    So each page (namespace? feature?) and it's routes are all bundled
    together neatly.

--- caveman/src/example/routes.clj
+++ caveman/src/example/routes.clj
@@ -9,32 +9,10 @@
             ; This is particularly useful when you want to refer to a namespace
             ; symbolically (e.g. qualified/namespaced keywords)
             [example.system :as-alias system]
+            [example.hello.routes :as hello-routes]
+            [example.goodbye.routes :as goodbye-routes]
             [clojure.tools.logging :as log]
-            [hiccup2.core :as hiccup]
-            [next.jdbc :as jdbc]))
-
-;; Destructures (associative destructuring) the `system` argument (namespaced keyword)
-;; such that the value is bound to the local name part of the key and drops the namespace.
-;; That is, `example.system/db` will be bound locally to `db`
-;; See:
-;; - <https://clojure.org/guides/destructuring#_associative_destructuring>
-;; - <https://clojure.org/guides/destructuring#_namespaced_keywords>
-(defn hello-handler [{::system/keys [db]} _request] ; Prefixing a parameter with underscore, or simply using an underscore as the name, is a convention that indicates the parameter is unused.
-  (let [{:keys [planet]} (jdbc/execute-one! db ["select 'earth' as planet"])]
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body (str (hiccup/html
-                 [:html
-                  [:body
-                   [:h1 "Hello, " planet]]]))}))
-
-(defn goodbye-handler [_system _request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (str (hiccup/html
-               [:html
-                [:body
-                 [:h1  "Goodbye, world!"]]]))})
+            [hiccup2.core :as hiccup]))
 
 ;; Unlike the other handlers, we're not passing `system` here.
 ;; At first, I incorrectly had `system` as a paramater, so I got an `ArityException`
@@ -45,8 +23,7 @@
 ;; We do this because system is how we get access to the database client.
 ;; And the handlers will need the db to fetch data
 (defn routes [system]
-  [["/"   {:get {:handler (partial #'hello-handler system)}}]
-   ["/goodbye"   {:get {:handler (partial #'goodbye-handler system)}}]])
+  ["" (hello-routes/routes system) (goodbye-routes/routes system)])
 
 ;; We're using var references to the handler functions. 
 ;; <https://clojure.org/guides/weird_characters#_var_quote>
