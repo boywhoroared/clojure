d2bd7d910738ce5fd56f7bcf0d4822c85f17bca8
Mario <developer@theboywhoroared.com>
2026-06-01 20:55:18 -0400

    Generate unit tests from the 4ever 4Clojure problem data using an LLM
    The generator can be run with:
    
    ```sh
    clojure -M -m boywhoroared.4clojure.generator
    ```

---
+++ 4clojure/src/boywhoroared/4clojure/generator.clj
@@ -0,0 +1,58 @@
+(ns boywhoroared.4clojure.generator
+  "Generates unit test files from 4Clojure problem data.
+  This module was LLM generated."
+  (:require [clojure.string :as string]
+            [clojure.pprint :refer [pprint]]
+            [clojure.java.io :as io]
+            [boywhoroared.4clojure.problems :refer [problems]]))
+
+(def test-dir "test/boywhoroared/4clojure")
+
+(defn sanitize-name [name-str]
+  (-> name-str
+      string/lower-case
+      (string/replace #"[^a-z0-9\s-]" "")
+      (string/replace #"\s+" "-")))
+
+(defn generate-test-ast [problem]
+  (let [id (:id problem)
+        safe-name (sanitize-name (:title problem))
+        ns-symbol (symbol (str "boywhoroared.4clojure.p" id "-" safe-name "-test"))
+        test-symbol (symbol (str "problem-" id "-test"))
+
+        ;; Map 4Clojure test strings into live Clojure forms inside (is ...) blocks
+        is-forms (map (fn [t-str] `(~'is ~(read-string t-str)))
+                      (concat (:tests problem) (:secret-tests problem)))]
+
+    ;; We return a vector of pure Clojure forms (the entire file's AST)
+    [`(~'ns ~ns-symbol
+        (:require [clojure.test :refer [~'deftest ~'is ~'testing]]))
+
+     `(~'defn ~'__ [& ~'args]
+        ~'(comment "Write your solution inside this function"))
+
+     `(~'deftest ~test-symbol
+        (~'testing ~(str "Problem " id ": " (:name problem))
+          ~@is-forms))]))
+
+(defn generate-test-file [problem]
+  (let [safe-name (sanitize-name (:title problem))
+        file-path (str test-dir "/p" (:id problem) "_" (string/replace safe-name "-" "_") "_test.clj")
+        ;; 1. Generate the structure of the file as data
+        file-ast (generate-test-ast problem)]
+
+    (io/make-parents file-path)
+    ;; 2. Spit the data natively using the pretty printer
+    (with-open [w (io/writer file-path)]
+      (binding [*out* w]
+        (doseq [form file-ast]
+          (pprint form)
+          (println)))))) ; Just one println to space out top-level forms cleanly
+
+(defn -main []
+  (println "Generating static test files natively from AST...")
+  (doseq [prob problems]
+    (generate-test-file prob))
+  (println (str  "Done! Check your " test-dir " directory.")))
+
+;; clojure -M -m boywhoroared.4clojure.generator
